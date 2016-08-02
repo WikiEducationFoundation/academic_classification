e49cb200a2f2d0fdfb15d8d6bbf417df40f1d2c0
Kevin Schiroo <kjschiroo@gmail.com>
2016-08-02 16:48:53 -0500

    init pageclassifier

---
+++ pageclassifier/pageclassifier.py
@@ -0,0 +1,28 @@
+import numpy as np
+from sklearn.svm import LinearSVC
+
+
+def _ensure_trained(func):
+    def wrapper(*args, **kwargs):
+        if args[0]._clf is None:
+            raise NotTrainedError
+        return func(*args, **kwargs)
+    return wrapper
+
+
+class PageClassifier(object):
+
+    def __init__(self, feature_extractors):
+        self._fx_exts = feature_extractors
+        self._clf = None
+
+    def train(self, wikicode_list, labels):
+        features = [ext.fit_extract(wikicode_list) for ext in self._fx_exts]
+        X = np.concatenate(features, axis=1)
+        self._clf = LinearSVC().fit(X, labels)
+
+    @_ensure_trained
+    def predict(self, wikicode_list):
+        features = [ext.extract(wikicode_list) for ext in self._fx_exts]
+        X = np.concatenate(features, axis=1)
+        return self._clf.predict(X)
